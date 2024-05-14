d3d90b67982357531e1d915de33625515114048e
Chandan Kumar <ck22abf@herts.ac.uk>
2024-05-14 20:43:27 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us-visa",
+    version="0.0.0",
+    author="Chandan",
+    author_email="chandankumarsingh1351@gmail.com/docker1351@outlook.com",
+    packages=find_packages()
+)
